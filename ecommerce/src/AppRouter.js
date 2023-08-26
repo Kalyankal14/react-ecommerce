a050e84186951c4d18cd1afaeaa2f9c65dc82dde
Zakeer Hussain <zakeer@zakeer.me>
2023-08-26 15:28:00 +0000

    firebase auth added

--- ecommerce/src/AppRouter.js
+++ ecommerce/src/AppRouter.js
@@ -3,14 +3,13 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
 import ProductsList from './components/ProductsList'
 import ProductDetails from './components/ProductDetails'
-import Login from './components/Login'
-import Signup from './components/Signup'
+import AuthenticationView from './components/AuthenticationView'
 
 export default function AppRouter({children}) {
     return <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login}/>
-        <Route path="/signup" component={Signup}/>
+        <Route path="/login" render={(routerProps) => <AuthenticationView {...routerProps} isLogin />}/>
+        <Route path="/signup" render={(routerProps) => <AuthenticationView {...routerProps} />}/>
         <Route exact path="/products" component={ProductsList} />
         <Route path="/products/:id" component={ProductDetails} />
     </Switch>
---
+++ ecommerce/src/firebaseApp.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwh3NHkAMlJ5cwnFgxIxSYPUQ2EYwjnvI",
+  authDomain: "react-ecommerce-657ee.firebaseapp.com",
+  projectId: "react-ecommerce-657ee",
+  storageBucket: "react-ecommerce-657ee.appspot.com",
+  messagingSenderId: "211794540032",
+  appId: "1:211794540032:web:e8116044ec6419d7b30ae5"
+};
+
+console.log(":: I am in Firebase App File ::");
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
